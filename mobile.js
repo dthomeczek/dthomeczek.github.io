39ffffcdea303b800aa982be8b1adfc596be18a4
dthomeczek <devinthomeczek@gmail.com>
2023-11-01 18:20:01 -0500

    added a script to make the mobile menu functional

---
+++ mobile.js
@@ -0,0 +1,10 @@
+// JavaScript to toggle the mobile menu
+document.addEventListener('DOMContentLoaded', function () {
+    const mobileMenu = document.getElementById('mobile-menu');
+    const navLinks = document.querySelector('.nav-links');
+
+    mobileMenu.addEventListener('click', function () {
+        navLinks.classList.toggle('active');
+        mobileMenu.classList.toggle('active');
+    });
+});
